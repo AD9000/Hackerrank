1076740ba007662a03588cb949fc76adb8a38387
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-01-24 22:47:37 +1100

    Create solveMeFirst.py
    Python code to create a function to add two numbers, 'a' and 'b'

---
+++ solveMeFirst.py
@@ -0,0 +1,16 @@
+
+
+def solveMeFirst(a,b):
+  // Simply add the two and return the value
+  return a + b
+
+
+// Accept the two numbers and store them in variables
+num1 = int(input())
+num2 = int(input())
+
+// Call function and solve problem (Store answer in variable called res)
+res = solveMeFirst(num1,num2)
+
+// Print solution
+print(res)
