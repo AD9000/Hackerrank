4d64be6b2f22e2c86434212cda57c7c1418e6bba
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-01-25 12:30:32 +1100

    Update solveMeFirst.py

--- solveMeFirst.py
+++ solveMeFirst.py
@@ -1,16 +1,14 @@
-
-
 def solveMeFirst(a,b):
-  // Simply add the two and return the value
+  # Simply add the two and return the value
   return a + b
 
 
-// Accept the two numbers and store them in variables
+# Accept the two numbers and store them in variables
 num1 = int(input())
 num2 = int(input())
 
-// Call function and solve problem (Store answer in variable called res)
+# Call function and solve problem (Store answer in variable called res)
 res = solveMeFirst(num1,num2)
 
-// Print solution
+# Print solution
 print(res)
