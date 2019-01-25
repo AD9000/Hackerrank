1f449a7f9b3c2941c4f23a7d22a345479e8c2c61
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-01-25 12:29:31 +1100

    Update percentage.py

--- percentage.py
+++ percentage.py
@@ -1,10 +1,14 @@
 if __name__ == '__main__':
+    # Accept input (As a dictionary)
     n = int(input())
     student_marks = {}
     for _ in range(n):
         name, *line = input().split()
         scores = list(map(float, line))
         student_marks[name] = scores
+        
+    # Accept name of student to be found
     query_name = input()
     
+    # Average students marks and print it. Formatting it to 2 decimal plaves. 
     print("%0.2f"%(sum(student_marks.get(query_name, None))/len(student_marks.get(query_name, None))))
