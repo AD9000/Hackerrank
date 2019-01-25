3acd7053acd5461057f7e915876dad8e0ef6928a
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-01-25 12:23:54 +1100

    Update NestedLists.py

--- NestedLists.py
+++ NestedLists.py
@@ -1,9 +1,13 @@
 if __name__ == '__main__':
     data = []
+    # Accept input in an empty list
     for _ in range(int(input())):
         data.append([input(), float(input())])
     
+    # Sort the first elements of each sublist and store in a set.
     scores = sorted({i[1] for i in data})
+    
+    # solution is a sorted list of names of people with the second highest score (ie scores[1]. scores[0] is highest)
     res = sorted([i[0] for i in data if i[1] == scores[1]])
     for i in res:
         print(i)
