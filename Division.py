03d5910df4ec571b62441de3faebf26f13d86e9d
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-01-25 12:40:27 +1100

    Create Division.py
    # Question Statement from Hackerrank (https://www.hackerrank.com/challenges/python-division/problem)
    
    Task 
    Read two integers and print two lines. The first line should contain integer division,  // . The second line should contain float division,  / .
    
    You don't need to perform any rounding or formatting operations.
    
    Input Format
    
    The first line contains the first integer, . The second line contains the second integer, .
    
    Output Format
    
    Print the two lines as described above.
    
    Sample Input 0
    
    4
    3
    Sample Output 0
    
    1
    1.33333333333

---
+++ Division.py
@@ -0,0 +1,12 @@
+# Author: Atharv Damle
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Integer divide
+    print(a//b)
+    
+    # Normal division
+    print(a/b)
+
