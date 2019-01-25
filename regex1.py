f1eac825bb294f6042c6674ba3a154f56021c08c
AD9000 <32605565+AD9000@users.noreply.github.com>
2019-01-25 12:30:07 +1100

    Update regex1.py

--- regex1.py
+++ regex1.py
@@ -3,22 +3,22 @@ import re
 s = input()
 k = input()
 
-// Input regex pattern (here, the string k)
+# Create regex pattern (here, the string k)
 p = re.compile(k)
 
-// Search for pattern in 's'
+# Search for pattern in 's'
 m = p.search(s)
 
-// If m is null,
+# If m is null,
 if not m:
     print ((-1, -1))
     
 else:
 
-    // While m exists,
+    # While m exists,
     while m:
         print((m.start(), m.end() - 1))
         
-        // Search again, skipping the first character of the last search
+        # Search again, skipping the first character of the last search
         m = p.search(s, m.start() + 1)
 
